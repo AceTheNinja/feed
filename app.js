9a19d73cac245776b80ba946d9821d5e370aed8d
AceTheNinja <raitapan1@gmail.com>
2016-09-16 12:55:55 +0530

    feeds

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿angular
+    .module("myApp", [])
+    .controller("mainController", function ($scope) {
+        $scope.posts = [];
+        $scope.edit = false;
+        $scope.del = false;
+        $scope.show = false;
+        $scope.newPost = { text: "" };
+        $scope.submit = function () {
+            $scope.posts.push($scope.newPost);
+            $scope.newPost = { text: "" };
+        };
+        $scope.del = function (x) {
+            x.text = "";
+            return x;
+        }
+    });
